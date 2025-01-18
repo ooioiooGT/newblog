02a71c4a41fa58103c5e5c29ec8898e32991bc48
ooioiooGT <97373570+ooioiooGT@users.noreply.github.com>
2025-01-17 23:12:22 -0700

    adding home page and about content

--- src/Page/HomePage.js
+++ src/Page/HomePage.js
@@ -1,6 +1,23 @@
+import { useNavigate } from "react-router-dom";
+
 const HomePage = () => { 
+    const navigate = useNavigate();
     return(
-        <h1> this is home page Test  </h1> 
+        <div className="home-page" >
+            <div className='home-text'>
+                {/* Hero Section */}
+                <div className="hero-section">
+                    <div className="hero-content">
+                        <h1>what will you do next? </h1>
+                        <p>"Life's journey isn’t just a path to follow—it's the one you create. Embrace your story and make it extraordinary."</p> 
+                    </div>
+                    <div>
+                        <button  onClick={()=>navigate('/articles')}>Explore My Blog</button>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 }
 export default HomePage;
+
